840c1646879e0362aa1613f7610a4ec040fb8aab
jw2511281 <jw2511281@sookmyung.ac.kr>
2025-10-02 15:32:50 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,17 +3,16 @@
 
 int main(int argc, char *argv[])
 {
-  int a;
+  int a, b;
   
   printf("정수 하나를 입력하시오 : ");
   scanf("%i", &a);
   
-  if ( a > 0 )
-     printf("양수입니다.");
-  else if ( a == 0 )
-     printf("0입니다.");
+  if ( a >= 0 )
+     printf("절대값은 %i 입니다.", a);
   else
-     printf("음수입니다."); 
+     b= -a ;
+     printf("절대값은 %i 입니다.", b); 
   
   system("PAUSE");	
   return 0;
